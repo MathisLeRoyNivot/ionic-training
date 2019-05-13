ff6cc23027e86893495323e1526746e15405a684
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-05-13 16:32:37 +0200

    Main project problem resolved

---
+++ myAppTraining/src/app/tab3/tab3.page.ts
@@ -0,0 +1,63 @@
+import { Component, ViewChild, Renderer } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  @ViewChild('myCanvas') canvas: any;
+
+  canvasElement: any;
+  lastX: number;
+  lastY: number;
+
+  currentColour: string = '#000';
+  brushSize: number = 10;
+
+
+  constructor(public platform: Platform, public renderer: Renderer) {
+    console.log('CanvasDraw Component');
+  }
+
+  ngAfterViewInit(){
+
+    this.canvasElement = this.canvas.nativeElement;
+
+    this.renderer.setElementAttribute(this.canvasElement, 'width', this.platform.width() + '');
+    this.renderer.setElementAttribute(this.canvasElement, 'height', this.platform.height() + '');
+
+  }
+
+  // First position of the line that the user draw
+  handleStart(ev) {
+    this.lastX = ev.touches[0].pageX;
+    this.lastY = ev.touches[0].pageY;
+    console.log("%cStarting", "color:green");
+  }
+
+  // When the user hold and move his finger accross the screen
+  handleMove(ev) {
+    let currentX = ev.touches[0].pageX;
+    let currentY = ev.touches[0].pageY;
+    console.log('%cNew point added',"color: green", '\nX position : ' + currentX + '\n' + 'Y position : ' + currentY);
+
+    let ctx = this.canvasElement.getContext('2d');
+
+    ctx.beginPath();
+        ctx.lineJoin = "round";
+        ctx.moveTo(this.lastX, this.lastY);
+        ctx.lineTo(currentX, currentY);
+        ctx.closePath();
+        ctx.strokeStyle = this.currentColour;
+        ctx.lineWidth = this.brushSize;
+        ctx.stroke();       
+
+        this.lastX = currentX;
+        this.lastY = currentY;
+
+  }
+
+}
