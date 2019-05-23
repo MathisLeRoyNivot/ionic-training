3622203b8e4be6642a1c43e4c101ac0f75d70f52
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-05-23 12:53:23 +0200

    App : Drawing App minor bugs fixed

--- drawingApp/src/app/home/home.page.ts
+++ drawingApp/src/app/home/home.page.ts
@@ -18,17 +18,30 @@ export class HomePage {
   constructor(
     public router: Router,
     private keyboard: Keyboard) {}
-  
-  logForm() {
-    this.keyboard.hide();
-    let formData = this.form;
-    console.log(formData);
-    
-    localStorage.clear();
-    localStorage.setItem('name', formData.name);
 
-    setTimeout(() => {
-      this.router.navigate(['/test']);
-    }, 50);
+
+  async keyboardStatus() {
+    if(this.keyboard.isVisible) {
+      this.keyboard.hide();
+      console.log("Keyboard closed !")
+    } else {
+      console.log("Keyboard already closed !");
+    }
   }
+
+  logForm() {
+
+    this.keyboardStatus().then(() => {
+      setTimeout(() => {
+        let formData = this.form;
+        console.log(formData);
+          
+        localStorage.clear();
+        localStorage.setItem('name', formData.name);
+      
+        this.router.navigate(['/test']);
+      }, 200)
+    });
+  };
+
 }
