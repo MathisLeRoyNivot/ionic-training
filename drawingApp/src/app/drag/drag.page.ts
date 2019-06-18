78ca8068e1fbc15bcb259b5dcfb6d3bbb600b7fe
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-06-18 16:35:46 +0200

    Dragable box working & start optimization

--- drawingApp/src/app/drag/drag.page.ts
+++ drawingApp/src/app/drag/drag.page.ts
@@ -1,8 +1,6 @@
-import { ViewChild, Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ViewChild, Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Location } from '@angular/common';
-import { Hammer } from 'hammerjs';
-import { container } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-drag',
@@ -13,16 +11,14 @@ export class DragPage implements OnInit {
 
   @ViewChild('topContainer') topContainer: any;
 
-  @Output() drag: EventEmitter<any> = new EventEmitter<any>();
-
   textElement: any;
 
   count: number = 0;
 
-  isContentBigger: Boolean = false;
-  sliderRangeScroll: number = 0;
+  drag: any;
+  dragStart: Boolean = false;
 
-  textContainer: any;
+  topTextContainer: any;
   textContentWrapper: any;
   textContent: any;
 
@@ -32,68 +28,84 @@ export class DragPage implements OnInit {
 
   ngOnInit() {}
 
-  ngOnDestroy(): void {
-    
-  }
-
   backClicked() {
     this.location.back();
   }
 
   ngAfterViewInit() {
-    this.textContainer = document.getElementById("top-container");
+    this.topTextContainer = document.getElementById("top-container");
     this.textContentWrapper = document.getElementById("text-content-wrapper");
     this.textContent = document.getElementById("text-content");
   }
-
-  onClick() {
-    this.drag.emit(5);
+  
+  startPan(ev) {
+    this.dragStart = true;
+    console.log("Starting draging !\nY : " + ev.center.y);
   }
+  
+  endPan(ev) {
+    let deviceHeight = this.platform.height();
+    let topNavHeight = this.textElement.getBoundingClientRect().top;
+    let minScroll = topNavHeight / deviceHeight * 100;
 
+    let yPos = ev.center.y - 40;
+    let viewRatio = yPos / deviceHeight;
+    let viewHeight = viewRatio * 100;
+
+    if(viewHeight >= minScroll && viewHeight <= 92) {
+      this.topTextContainer.style.height = viewHeight + "vh";
+      this.textContentWrapper.style.height = viewHeight + "vh";
+      console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
+    } else if(viewHeight > 92) {
+      this.topTextContainer.style.height = "92vh";  
+    } else if(viewHeight < minScroll) {
+      this.topTextContainer.style.height = minScroll + "vh";
+    }
+
+    console.log("Drag ended !\nY : " + ev.center.y);
+    if(this.dragStart) {
+      this.dragStart = false;
+      clearInterval(this.drag);
+    }
+  }
+  
   handlePan(ev) {
     
     ev.preventDefault();
-
+    
     this.textElement = this.topContainer.nativeElement;
     let topNavHeight = this.textElement.getBoundingClientRect().top;
     let deviceHeight = this.platform.height();
-
-    // Get position coordinates
-    let xyPos = ev.center;
-    let xPos = ev.center.x;
+    
     let yPos = ev.center.y - 40;
-
-    // Size of the text content
+    
     let contentHeight = this.textContent.offsetHeight;
     let contentRatio = (contentHeight + 70) / deviceHeight * 100;
-
-    // Total device view size
+    
     let viewRatio = yPos / deviceHeight;
     let viewHeight = viewRatio * 100;
     let minScroll = topNavHeight / deviceHeight * 100;
 
-    if(contentRatio >= viewHeight) {
-      this.isContentBigger = true;
-      this.sliderRangeScroll = 0;
-    } else if(contentRatio < viewHeight) {
-      this.isContentBigger = false;
-    }
-
     if(viewHeight >= minScroll && viewHeight <= 92) {
-      this.textContainer.style.height = viewHeight + "vh";
-      // this.textContentWrapper.style.height= "calc("+ viewHeight + "vh - 2em)";
+      this.topTextContainer.style.height = viewHeight + "vh";
       this.textContentWrapper.style.height = viewHeight + "vh";
-
-      // this.textContent.style.height = viewHeight + "vh";
-
       console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
     } else if(viewHeight > 92) {
-      this.textContainer.style.height = "92vh";  
+      this.topTextContainer.style.height = "92vh";  
     } else if(viewHeight < minScroll) {
-      this.textContainer.style.height = minScroll + "vh";
+      this.topTextContainer.style.height = minScroll + "vh";
     }
+    
+    // if(this.dragStart) {
+    //   this.drag = setInterval(() => {
+    //     // this.topTextContainer.style.height = viewHeight + "vh";
+    //     this.textContentWrapper.style.height = viewHeight.toFixed(1) + "vh";
+    //     console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
+    //   }, 20);
+    //   clearInterval(this.drag);
+    // }
   }
-
+  
   handleDoubleTap(ev) {
     ev.preventDefault();
 
@@ -103,30 +115,11 @@ export class DragPage implements OnInit {
         this.count = 0;
 
       } else if(this.count == 2){
-        // If user double tap then it reset the height to the default value of $0vh
         this.count = 0;
-        this.isContentBigger = true;
-        this.sliderRangeScroll = 0;
         console.log('Double Tap : Top-container height has been reset.');
-        this.textContainer.style.height = "40vh";
+        this.topTextContainer.style.height = "40vh";
         this.textContentWrapper.style.height = "40vh";
-        this.textContent.style.height = "40vh";
       }
     }, 250);
   }
-
-  // newScrollValue(ev) {
-    
-  //   let percent = ev / 100;
-
-  //   let scrollableDiv = this.textContent
-  //   let topContainerSize = this.textContainer.offsetHeight;
-    
-  //   let diffSize = scrollableDiv.scrollHeight - topContainerSize;
-  //   let diffSizeRatio = diffSize * percent + percent * 65;
-  //   console.log(diffSizeRatio);
-  //   document.getElementById("text-content-wrapper").scrollTop = diffSizeRatio;   
-
-  // }
-  
 }
