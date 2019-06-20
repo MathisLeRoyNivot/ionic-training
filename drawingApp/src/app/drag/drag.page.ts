ddb85f9271ed3d58f2335e1db222c73546b07d8c
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-06-20 09:47:44 +0200

    Minor changes on style

--- drawingApp/src/app/drag/drag.page.ts
+++ drawingApp/src/app/drag/drag.page.ts
@@ -1,125 +1,119 @@
-import { ViewChild, Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { Location } from '@angular/common';
+import { ViewChild, Component, OnInit } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-drag',
-  templateUrl: './drag.page.html',
-  styleUrls: ['./drag.page.scss'],
+    selector: "app-drag",
+    templateUrl: "./drag.page.html",
+    styleUrls: ["./drag.page.scss"]
 })
 export class DragPage implements OnInit {
+    @ViewChild("topContainer") topContainer: any;
 
-  @ViewChild('topContainer') topContainer: any;
+    textElement: any;
 
-  textElement: any;
+    count: number = 0;
 
-  count: number = 0;
+    drag: any;
+    dragStart: Boolean = false;
 
-  drag: any;
-  dragStart: Boolean = false;
+    topTextContainer: any;
+    textContentWrapper: any;
+    textContent: any;
 
-  topTextContainer: any;
-  textContentWrapper: any;
-  textContent: any;
+    constructor(public platform: Platform, public location: Location) { }
 
-  constructor(
-    public platform: Platform,
-    public location: Location) {}
+    ngOnInit() { }
 
-  ngOnInit() {}
-
-  backClicked() {
-    this.location.back();
-  }
-
-  ngAfterViewInit() {
-    this.topTextContainer = document.getElementById("top-container");
-    this.textContentWrapper = document.getElementById("text-content-wrapper");
-    this.textContent = document.getElementById("text-content");
-  }
-  
-  startPan(ev) {
-    this.dragStart = true;
-    console.log("Starting draging !\nY : " + ev.center.y);
-  }
-  
-  endPan(ev) {
-    let deviceHeight = this.platform.height();
-    let topNavHeight = this.textElement.getBoundingClientRect().top;
-    let minScroll = topNavHeight / deviceHeight * 100;
-
-    let yPos = ev.center.y - 40;
-    let viewRatio = yPos / deviceHeight;
-    let viewHeight = viewRatio * 100;
-
-    if(viewHeight >= minScroll && viewHeight <= 92) {
-      this.topTextContainer.style.height = viewHeight + "vh";
-      this.textContentWrapper.style.height = viewHeight + "vh";
-      console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
-    } else if(viewHeight > 92) {
-      this.topTextContainer.style.height = "92vh";  
-    } else if(viewHeight < minScroll) {
-      this.topTextContainer.style.height = minScroll + "vh";
+    backClicked() {
+        this.location.back();
     }
 
-    console.log("Drag ended !\nY : " + ev.center.y);
-    if(this.dragStart) {
-      this.dragStart = false;
-      clearInterval(this.drag);
+    ngAfterViewInit() {
+        this.topTextContainer = document.getElementById("top-container");
+        this.textContentWrapper = document.getElementById("text-content-wrapper");
+        this.textContent = document.getElementById("text-content");
     }
-  }
-  
-  handlePan(ev) {
-    
-    ev.preventDefault();
-    
-    this.textElement = this.topContainer.nativeElement;
-    let topNavHeight = this.textElement.getBoundingClientRect().top;
-    let deviceHeight = this.platform.height();
-    
-    let yPos = ev.center.y - 40;
-    
-    let contentHeight = this.textContent.offsetHeight;
-    let contentRatio = (contentHeight + 70) / deviceHeight * 100;
-    
-    let viewRatio = yPos / deviceHeight;
-    let viewHeight = viewRatio * 100;
-    let minScroll = topNavHeight / deviceHeight * 100;
 
-    if(viewHeight >= minScroll && viewHeight <= 92) {
-      this.topTextContainer.style.height = viewHeight + "vh";
-      this.textContentWrapper.style.height = viewHeight + "vh";
-      console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
-    } else if(viewHeight > 92) {
-      this.topTextContainer.style.height = "92vh";  
-    } else if(viewHeight < minScroll) {
-      this.topTextContainer.style.height = minScroll + "vh";
+    startPan(ev) {
+        this.dragStart = true;
+        console.log("%cStarting draging !\nY : " + ev.center.y, "color: #6ab04c");
     }
-    
-    // if(this.dragStart) {
-    //   this.drag = setInterval(() => {
-    //     // this.topTextContainer.style.height = viewHeight + "vh";
-    //     this.textContentWrapper.style.height = viewHeight.toFixed(1) + "vh";
-    //     console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
-    //   }, 20);
-    //   clearInterval(this.drag);
-    // }
-  }
-  
-  handleDoubleTap(ev) {
-    ev.preventDefault();
 
-    this.count++;
-    setTimeout(() => {
-      if (this.count == 1) {
-        this.count = 0;
+    endPan(ev) {
+        let deviceHeight = this.platform.height();
+        let topNavHeight = this.textElement.getBoundingClientRect().top;
+        let minScroll = (topNavHeight / deviceHeight) * 100;
 
-      } else if(this.count == 2){
-        this.count = 0;
-        console.log('Double Tap : Top-container height has been reset.');
-        this.topTextContainer.style.height = "40vh";
-        this.textContentWrapper.style.height = "40vh";
-      }
-    }, 250);
-  }
+        let yPos = ev.center.y - 40;
+        let viewRatio = yPos / deviceHeight;
+        let viewHeight = viewRatio * 100;
+
+        if (viewHeight >= minScroll && viewHeight <= 92) {
+            this.topTextContainer.style.height = viewHeight + "vh";
+            this.textContentWrapper.style.height = viewHeight + "vh";
+        } else if (viewHeight > 92) {
+            this.topTextContainer.style.height = "92vh";
+        } else if (viewHeight < minScroll) {
+            this.topTextContainer.style.height = minScroll + "vh";
+        }
+
+        console.log("%cDrag ended !\nY : " + ev.center.y, "color: #eb2f06");
+        if (this.dragStart) {
+            this.dragStart = false;
+            clearInterval(this.drag);
+        }
+    }
+
+    handlePan(ev) {
+        ev.preventDefault();
+
+        this.textElement = this.topContainer.nativeElement;
+        let topNavHeight = this.textElement.getBoundingClientRect().top;
+        let deviceHeight = this.platform.height();
+
+        let yPos = ev.center.y - 40;
+
+        let contentHeight = this.textContent.offsetHeight;
+        let contentRatio = ((contentHeight + 70) / deviceHeight) * 100;
+
+        let viewRatio = yPos / deviceHeight;
+        let viewHeight = viewRatio * 100;
+        let minScroll = (topNavHeight / deviceHeight) * 100;
+
+        if (viewHeight >= minScroll && viewHeight <= 92) {
+            this.topTextContainer.style.height = viewHeight + "vh";
+            this.textContentWrapper.style.height = viewHeight + "vh";
+            console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
+        } else if (viewHeight > 92) {
+            this.topTextContainer.style.height = "92vh";
+        } else if (viewHeight < minScroll) {
+            this.topTextContainer.style.height = minScroll + "vh";
+        }
+
+        // if(this.dragStart) {
+        //   this.drag = setInterval(() => {
+        //     // this.topTextContainer.style.height = viewHeight + "vh";
+        //     this.textContentWrapper.style.height = viewHeight.toFixed(1) + "vh";
+        //     console.log("Dragable container : " + viewHeight.toFixed(3) + "%");
+        //   }, 20);
+        //   clearInterval(this.drag);
+        // }
+    }
+
+    handleDoubleTap(ev) {
+        ev.preventDefault();
+
+        this.count++;
+        setTimeout(() => {
+            if (this.count == 1) {
+                this.count = 0;
+            } else if (this.count == 2) {
+                this.count = 0;
+                console.log("Double Tap : Top-container height has been reset.");
+                this.topTextContainer.style.height = "40vh";
+                this.textContentWrapper.style.height = "40vh";
+            }
+        }, 250);
+    }
 }
